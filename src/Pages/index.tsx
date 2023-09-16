d5eeba942af0a6fa5bef56ae967ac9952b4b33da
CristianWhitesoft <cristian.badillo@whitesoft.com.au>
2023-09-16 15:50:06 -0500

    Edit cars

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -2,8 +2,9 @@ import Login from './Auth/Login'
 import Register from './Auth/Register'
 import Error404 from './Error404'
 import Home from './Home'
-import NuevoAuto from './NewCar'
-import TableCars from './TableCars'
+import NuevoAuto from './Cars/NewCar'
+import TableCars from './Cars/TableCars'
+import EditEncuesta from './Cars/EditCar'
 
 export const error404 = <Error404 />
 export const login = <Login />
@@ -11,3 +12,4 @@ export const register = <Register />
 export const home = <Home />
 export const newCar = <NuevoAuto />
 export const tableCars = <TableCars />
+export const editCar = <EditEncuesta />
